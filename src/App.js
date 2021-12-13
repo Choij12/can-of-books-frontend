a1bf3e041cfe6347cc1eb1e5098d520ad55ceff4
Jacob Choi <jacob.j.choi@gmail.com>
2021-12-13 14:01:15 -0800

    added template codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    }
+  }
+
+  loginHandler = (user) => {
+    this.setState({
+      user,
+    })
+  }
+
+  logoutHandler = () => {
+    this.setState({
+      user: null,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <Header user={this.state.user} onLogout={this.logoutHandler} />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+          </Switch>
+          <Footer />
+        </Router>
+      </>
+    )
+  }
 }
 
 export default App;
